063a7f5979263dc77e6e89cd4dd56d6a958cffb2
Nariman Azamatov <azamatov.nariman@jits-4-17.jits.ipa.dc>
2022-07-05 11:25:59 +0600

    ads

---
+++ most_repeated/index.ts
@@ -0,0 +1,16 @@
+export default function most_repeated(word) {
+  const wordtwo = word.replace(/[^a-zA-Z1-9]/g, "");
+  let countMax = 0;
+  for (let i = 0; i < wordtwo.length; i++) {
+    let count = 0;
+    for (let j = i; j < wordtwo.length; j++) {
+      if (wordtwo[i] === wordtwo[j]) {
+        count++;
+      }
+    }
+    if (count > countMax) {
+      countMax = count;
+    }
+  }
+  return countMax;
+}
