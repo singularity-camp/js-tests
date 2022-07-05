5991a839eaa0d311ac03a35963aa66c83944d78c
Talgat <talgat.s@protonmail.com>
2022-07-05 11:59:27 +0600

    Update farm tests

--- farm/index.test.ts
+++ farm/index.test.ts
@@ -4,15 +4,21 @@ describe("test countLegs", () => {
   const farmTable = [
     { chickens: 4, cows: 2, pigs: 1, expected: 20 },
     { chickens: 2, cows: 1, pigs: 2, expected: 16 },
-    { chickens: -2, cows: 1, pigs: 2, expected: "chickens are not valid" },
-    { chickens: 2, cows: -1, pigs: 2, expected: "cows are not valid" },
-    { chickens: 2, cows: 1, pigs: -2, expected: "pigs are not valid" },
+    // { chickens: -2, cows: 1, pigs: 2, expected: "chickens are not valid" },
+    // { chickens: 2, cows: -1, pigs: 2, expected: "cows are not valid" },
+    // { chickens: 2, cows: 1, pigs: -2, expected: "pigs are not valid" },
   ];
   test.each(farmTable)(
-    "checks countlegs ($chickens, $cows, $pigs) $expected",
+    "checks success countlegs ($chickens, $cows, $pigs) $expected",
     ({ chickens, cows, pigs, expected }) => {
       const result = countLegs(chickens, cows, pigs);
       expect(result).toBe(expected);
     }
   );
+
+  test("checks fail countlegs for chickens", () => {
+    expect(() => countLegs(-2, 1, 2)).toThrowError(
+      new Error("chickens are not valid")
+    );
+  });
 });
--- farm/index.ts
+++ farm/index.ts
@@ -1,24 +1,22 @@
-function countLegs(
-    chickens: number,
-    cows: number,
-    pigs: number
-  ): number | string {
-    let result = 0;
-  
-    if (chickens > 0) {
-      result += chickens * 2;
-    } else {
-      return "chickens are not valid";
-    }
-    if (cows > 0) {
-      result += cows * 4;
-    } else {
-      return "cows are not valid";
-    }
-    if (pigs > 0) {
-      result += pigs * 4;
-    } else {
-      return "pigs are not valid";
-    }
-    return result;
+function countLegs(chickens: number, cows: number, pigs: number): number {
+  let result = 0;
+
+  if (chickens > 0) {
+    result += chickens * 2;
+  } else {
+    throw new Error("chickens are not valid");
   }
+  if (cows > 0) {
+    result += cows * 4;
+  } else {
+    throw new Error("cows are not valid");
+  }
+  if (pigs > 0) {
+    result += pigs * 4;
+  } else {
+    throw new Error("pigs are not valid");
+  }
+  return result;
+}
+
+export default countLegs;
