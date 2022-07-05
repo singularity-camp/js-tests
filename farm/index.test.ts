806724e13e660814da87061eed4c1c02c1d419b1
Ilyas Bekpayev <ilyas.bekpayev@jits-4-18.jits.ipa.dc>
2022-07-05 11:48:35 +0600

    expanded farm test

--- farm/index.test.ts
+++ farm/index.test.ts
@@ -4,6 +4,9 @@ describe("test countLegs", () => {
   const farmTable = [
     { chickens: 4, cows: 2, pigs: 1, expected: 20 },
     { chickens: 2, cows: 1, pigs: 2, expected: 16 },
+    { chickens: -2, cows: 1, pigs: 2, expected: "chickens are not valid" },
+    { chickens: 2, cows: -1, pigs: 2, expected: "cows are not valid" },
+    { chickens: 2, cows: 1, pigs: -2, expected: "pigs are not valid" },
   ];
   test.each(farmTable)(
     "checks countlegs ($chickens, $cows, $pigs) $expected",
